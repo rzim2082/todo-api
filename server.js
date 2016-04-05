3098505b9c98d8cf5f5f8293f9ed5e6f8b5b8caa
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-05 14:33:16 -0400

    Init repo

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+
+app.get('/', function(req, res){
+	res.send('Todo API Root');
+});
+
+
+app.listen(port, function(){
+	console.log('Express listening on ' + port);
+})
