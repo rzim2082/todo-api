3ca2fa443f4f8afd3597bddbb93110c137e70679
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-09 23:28:23 -0400

    Add user model and post users request

--- server.js
+++ server.js
@@ -215,6 +215,17 @@ app.put('/todos/:id', function(req, res) {
 
 });
 
+app.post('/users', function(req, res){
+	var body = _.pick(req.body, 'email', 'password');
+
+	db.user.create(body).then(function(todo){
+		res.json(user.toJSON());
+	}, function(e){
+		res.status(400).json(e);
+	});
+
+});
+
 db.sequelize.sync().then(function(){
 	app.listen(port, function() {
 	console.log('Express listening on ' + port);
