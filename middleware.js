a4fa3ed132aff4ffba496bca2070932a7332fc5d
Ryan Zimmerman <ryanwilliamzimmerman@gmail.com>
2016-04-11 14:49:19 -0400

    added delete users/login

--- middleware.js
+++ middleware.js
@@ -1,9 +1,31 @@
+var cryptojs = require('crypto-js');
+
 module.exports = function (db) { //reason we set it to a function is so other files can pass in config data
 
 	return {
 		requireAuthentication: function(req, res, next){
-			var token = req.get('Auth');
+			var token = req.get('Auth') || ''; //keeps from erroing out
 
+			db.token.findOne({
+				where: {
+					tokenHash: cryptojs.MD5(token).toString()
+				}
+			}).then(function(tokenInstance){
+				if(!tokenInstance){
+					throw new Error();
+				}
+
+				req.token = tokenInstance;
+				return db.user.findByToken(token);
+
+			}).then(function(user){
+				req.user = user;
+				next();
+			}).catch(function(){
+				res.status(401).send();
+			});
+
+			/*
 			db.user.findByToken(token).then(function (user) {
 				req.user = user;
 				next();
@@ -11,6 +33,7 @@ module.exports = function (db) { //reason we set it to a function is so other fi
 				
 				res.status(401).send();
 			});
+			*/
 		}
 	};
 
